0ab36eaf20fbc5fcfabf01fc70abcdd48fe5c4a1
Caio Oliveira <caio.oliveira4@nextel.com.br>
2019-02-21 19:22:03 -0300

    created basic structure of home view

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  changeView (route) {
+    console.log('do nothing')
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image style={styles.img} source={{url: 'https://via.placeholder.com/140x100'}} />
+        <Text style={styles.text}>Welcome to RN Training</Text>
+        <TouchableOpacity 
+          onPress={this.changeView}
+          style={styles.button}
+        >
+          <Text>Enter</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  img: {
+    width: 140,
+    height: 100,
+    marginBottom: 20,
+  },
+  text: {
+    marginBottom:20,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
